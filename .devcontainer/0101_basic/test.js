ed799a27ede345bf060a8a5ce789273bc78a89f2
BipinAchaRya21 <bipinacharya981@gmail.com>
2026-01-08 03:54:14 +0000

    datatypes operations

--- .devcontainer/0101_basic/test.js
+++ .devcontainer/0101_basic/test.js
@@ -1,1 +1,30 @@
-console.log("Bipin");
+//datatype conversion
+/*let score="33"
+console.log(typeof (score));
+let valueInNumber=Number (score)
+console.log(typeof score);
+console.log(valueInNumber);
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+let someNumber=33
+let stringNumber=string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);*/
+//javascript operations
+/*let value=3
+let negvalue=-value
+console.log(negvalue);
+console.log(2+2);
+let str1="Hello"
+let str2="Bipin"
+let str3=str1+str2
+console.log(str3);
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+2+"2");*/
+//javascript (comparison of datatypes)
+/*console.log(2>1);
+console.log("2">1);
+console.log(null>0);
+console.log("2"==2);//=== datatype check*/
