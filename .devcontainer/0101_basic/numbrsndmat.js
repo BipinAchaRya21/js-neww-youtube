ca7ae5fe0c93bd503be19c366f8c8ddb4e97e0e7
BipinAchaRya21 <bipinacharya981@gmail.com>
2026-01-12 13:50:21 +0000

    stringandmath

---
+++ .devcontainer/0101_basic/numbrsndmat.js
@@ -0,0 +1,24 @@
+//++++++++++Strings++++++++++
+/*const score=400
+const balance = new Number(100)
+console.log(score);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+const otherNumber=123.8977
+console.log(otherNumber.toPrecision(4));
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-IN'));*/
+//++++++++++++++++Maths++++++++++++
+/*console.log(Math);
+console.log(Math.abs(-8));
+console.log(Math.round(4.33));
+console.log(Math.ceil(5.2));
+console.log(Math.floor(5.9));
+console.log(Math.min(4,5,3,9));
+console.log(Math.max(9,0,4,5));*/
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1)+min));
