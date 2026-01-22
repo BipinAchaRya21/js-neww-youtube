2f2b27a8dd9dff6853f20c81664f9df1f284a062
BipinAchaRya21 <bipinacharya981@gmail.com>
2026-01-22 13:41:18 +0000

    scope and mini hosting

--- .devcontainer/03_basics/02_scope.js
+++ .devcontainer/03_basics/02_scope.js
@@ -1,4 +1,4 @@
-let a=344
+/*let a=344
 const b=99
 //var c=33
 if(true)
@@ -14,4 +14,42 @@ console.log(b);
 /* for(let i=0;i<array.length;i++)
 {
 const element=array[i];
+}*/
+
+// scope level and mini hosting in js
+function one()
+{
+    const username="Bipin"
+    function two()
+    {
+        const website="youtube"
+        console.log(username);
+    }
+    //console.log(website);
+   // two()
 }
+//one()
+if(true)
+{
+    const username="Hitesh"
+    if(username==="Hitesh")
+    {
+        const website="  Youtube"
+        //console.log(username+website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+//++++++++++Interesting+++++++
+//console.log(addOne(5));//yes printed before declare
+function addOne(num)
+{
+return num+1
+}
+//console.log(addOne(5));
+console.log(addTwo(5));
+const addTwo=function(num) 
+{
+    return num+2
+}
+//console.log(addTwo(5));
