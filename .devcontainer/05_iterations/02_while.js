9153b3d126b2fc9d504074facb9f0b211557899b
BipinAchaRya21 <bipinacharya981@gmail.com>
2026-01-27 13:50:11 +0000

    while and do while loop

---
+++ .devcontainer/05_iterations/02_while.js
@@ -0,0 +1,20 @@
+//while loop
+let index=0
+while(index<=10)
+{
+    //console.log(`value of index is:${index}`);
+    index=index+2;
+}
+let arr=0
+myArray=["flash","superman","batman"]
+while(arr<myArray.length)
+{
+   // console.log(`value of index ${arr}:${myArray[arr]}`);
+    arr=arr+1;
+}
+// do while loop
+let score=11
+do{
+    console.log(`score is:${score}`);
+    score++
+} while(score<=10)
