7e0a799426a9ec6d9a29c5355303952f127668c4
BipinAchaRya21 <bipinacharya981@gmail.com>
2026-01-28 12:02:00 +0000

    high arrray concept

---
+++ .devcontainer/05_iterations/higarray.js
@@ -0,0 +1,32 @@
+//for of loop
+//["","",""]
+//[{},{},{}]
+const arr=[1,2,3,4,5]
+for(const num of arr)
+{
+    //console.log(`value is:${num}`)
+}
+const greeting="Hello World!"
+for(const greet of greeting)
+{
+   // console.log(`character is:${greet}`);
+}
+// maps ->unique key value pairs no duplicate keys
+/*const map=new Map()
+map.set('IN',"India")
+map.set('US',"United States")
+map.set('UK',"United Kingdom")
+map.set('NP',"Nepal")
+map.set('US',"United States")
+//console.log(map);
+for(const[key,value]of ap){//map ma kam gardaina
+    //console.log(key,":-",value);
+}*/
+/*const myObject={
+    'game1':'football',
+    'game2':'cricker'
+}
+for(const[key,value]of myObject){
+    console.log(key,":-",value);
+}*/
+
