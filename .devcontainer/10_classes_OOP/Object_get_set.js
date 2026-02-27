d4dc0979d54d20b493fd27860c943ae7f1a1600a
BipinAchaRya21 <bipinacharya981@gmail.com>
2026-02-27 13:23:23 +0000

    last object get set

---
+++ .devcontainer/10_classes_OOP/Object_get_set.js
@@ -0,0 +1,12 @@
+const user={
+    _email:"bipin@gmail.com",
+    _password:"234",
+    get email(){
+        return this._email.toUpperCase();
+    },
+    set email(value){
+        this._email=value;
+    }
+}
+const tea=Object.create(user);
+console.log(tea.email);
